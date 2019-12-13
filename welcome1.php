494c518256e726af45b93d26f0c71c07b88abd6c
Snahva Labs <52474144+snahvalabs@users.noreply.github.com>
2019-12-13 14:26:51 +0700

    UI Main Page

---
+++ welcome1.php
@@ -0,0 +1,20 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bank Hore</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Welcome to Bank Hore</h1>
+    </div>
+    <p>
+        <a href="register.php" class="btn btn-warning">Register</a>
+        <a href="login.php" class="btn btn-danger">Login</a>
+    </p>
+</body>
+</html>
